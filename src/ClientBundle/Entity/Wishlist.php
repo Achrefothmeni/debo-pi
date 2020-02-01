b7d1b655202e9355a102b946cb5d214adbaaaaeb
Achrefothmeni <othmeniachref@gmail.com>
2020-02-01 12:36:34 +0100

    add database configuration

---
+++ src/ClientBundle/Entity/Wishlist.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ClientBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Wishlist
+ *
+ * @ORM\Table(name="wishlist")
+ * @ORM\Entity(repositoryClass="ClientBundle\Repository\WishlistRepository")
+ */
+class Wishlist
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
