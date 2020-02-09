a80173e3a832a4e86e2cf15742f40de054e4072e
Achrefothmeni <othmeniachref@gmail.com>
2020-02-09 17:53:15 +0100

    integrated FOSUserBundle

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -66,6 +66,7 @@ class ComposerStaticInit073d87c53c1e8125dc40d928bc125959
         'F' => 
         array (
             'Fig\\Link\\' => 9,
+            'FOS\\UserBundle\\' => 15,
         ),
         'D' => 
         array (
@@ -199,6 +200,10 @@ class ComposerStaticInit073d87c53c1e8125dc40d928bc125959
         'Fig\\Link\\' => 
         array (
             0 => __DIR__ . '/..' . '/fig/link-util/src',
+        ),
+        'FOS\\UserBundle\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/friendsofsymfony/user-bundle',
         ),
         'Doctrine\\Instantiator\\' => 
         array (
