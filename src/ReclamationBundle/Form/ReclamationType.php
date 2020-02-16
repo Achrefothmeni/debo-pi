dd3d37a99bb97f0ca4fb021a143a8b621d5951a6
Dhia_jml <dhiaeddine.jemaiel@esprit.tn>
2020-02-16 16:10:02 +0100

    fix ajout

--- src/ReclamationBundle/Form/ReclamationType.php
+++ src/ReclamationBundle/Form/ReclamationType.php
@@ -3,6 +3,7 @@
 namespace ReclamationBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,9 @@ class ReclamationType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('message')->add('type');
+        $builder->add('message')
+            ->add('type')
+            ->add('Envoyer', SubmitType::class);
     }/**
      * {@inheritdoc}
      */
